89b1c06db34f87217c42d21f092c08a9803819cf
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-09-29 11:44:37 +0600

    card btn working

--- src/components/Home/StudyInfo/StudyInfo.js
+++ src/components/Home/StudyInfo/StudyInfo.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 const StudyInfo = ({ breaks, time, setTime }) => {
-    // const newTime = [time];
-    // // newTime.push(time)
-    // const upTime = [...newTime]
-    // console.log(upTime)
+    console.log(time)
+    let total = 0;
+    for (const times of time) {
+        total = total + times.Time;
+    }
     return (
         <div>
             <h4 className='mt-5 mb-4'>Study Details</h4>
             <div className='d-flex justify-content-between mb-4'>
                 <h6>Study Time</h6>
-                <h6>15min</h6>
+                <h6>{total}min</h6>
             </div>
             <div className='d-flex justify-content-between'>
                 <h6>Break Time</h6>
